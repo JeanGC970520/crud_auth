224f9e6531993487f57383b1d4912cbe9110924d
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-12-29 15:05:22 -0600

    Execute migrate command, creating signup form and User; using form and model from Django

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm # * Formulario proporcionado por Django
+from django.contrib.auth.models import User # * Modelo proporcionado ya por Django. Aunque podemos hacer los nuestros
+from django.http import HttpResponse
 # Create your views here.
 
-def helloWorld(request):
-    return render(request, 'signup.html', {
-        'form' : UserCreationForm(),
-    })
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form' : UserCreationForm(),
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            # Register user
+            try:
+                # Creando User
+                user =User.objects.create_user(
+                    username=request.POST['username'], 
+                    password=request.POST['password1']
+                )
+                # Guardando User en DB
+                user.save()
+                return HttpResponse('User created successfully')
+            except Exception as e:
+                print(e)
+                return HttpResponse('Username already exists')
+        return HttpResponse('Password do not match')
