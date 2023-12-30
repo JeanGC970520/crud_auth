dd3038cf16e2393bff8e76b468b3fcc762ef94cb
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-12-30 14:18:56 -0600

    Doing use of an AdminModel to show an especific Model fields like readonly

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Task
 
+# * Clase que permitira al Admin visualizar campos como solo de lectura
+class TaskAdmin(admin.ModelAdmin):
+    # * IMPORTANTE: Los campos deben llamarse igual que las propiedades del Modelo y ser un Tupla
+    readonly_fields = ("created", )
+
 # Register your models here.
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
