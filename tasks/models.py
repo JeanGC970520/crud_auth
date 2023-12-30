937444350c704ff24278dd80336136763ecb4668
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-12-30 14:13:05 -0600

    Registering Task model in the Administrator panel

--- tasks/models.py
+++ tasks/models.py
@@ -9,3 +9,6 @@ class Task(models.Model):
     dateCompleted = models.DateTimeField()
     important = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} - by {self.user.username}"
